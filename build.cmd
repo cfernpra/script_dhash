a58517454264151be67f38967c22afdca9109991
carlos fernandez <cfernande1408@gmail.com>
2025-04-14 18:39:50 +0200

    Release v1.0.0

---
+++ build.cmd
@@ -0,0 +1,4 @@
+@echo off
+echo Building dHashChanger.exe...
+C:\Users\cfern\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\pyinstaller.exe --onefile dHashChanger.py
+pause
